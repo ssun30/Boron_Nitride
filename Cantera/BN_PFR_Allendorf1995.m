6a639998ab77cf37daf67770a3be7b45fe06c56a
ssun30 <sun.su@northeastern.edu>
2022-10-26 18:04:57 -0400

    Combined Plug_FLow_1 and Plug_Flow_2 into a single function.

--- Cantera/BN_PFR_Allendorf1995.m
+++ Cantera/BN_PFR_Allendorf1995.m
@@ -31,9 +31,7 @@ t0 = cputime;
 
 T_array1 = linspace(1000, 2000, 21);
 P0 = 760 * 133.32;
-X0 = zeros(1, g.nSpecies);
-X0(g.speciesIndex('BCL3')) = 0.05;
-X0(g.speciesIndex('H2')) = 0.95;
+X0 = 'BCL3:0.05,H2:0.95';
 
 X_BCL3 = [];
 X_BCL2 = [];
@@ -46,12 +44,11 @@ X_H2 = [];
 for i = 1:length(T_array1)
     fprintf('Solving for temperature = %d\n', T_array1(i));
     g.TPX = {T_array1(i), P0, X0};
-    g.equilibrate('HP');
-    output = Plug_Flow_1(g, {'BCL3', 'BCL2', 'BCL', 'HCL'});
-    X_BCL3 = [X_BCL3, output(1)];
-    X_BCL2 = [X_BCL2, output(2)];
-    X_BCL = [X_BCL, output(3)];
-    X_HCL = [X_HCL, output(4)];
+    output = Plug_Flow(g, {'BCL3', 'BCL2', 'BCL', 'HCL'}, 1.0);
+    X_BCL3 = [X_BCL3, output(2, end)];
+    X_BCL2 = [X_BCL2, output(3, end)];
+    X_BCL = [X_BCL, output(4, end)];
+    X_HCL = [X_HCL, output(5, end)];
 end
 
 T_array2 = linspace(800, 2000, 25);
@@ -63,17 +60,16 @@ X_NH3 = [];
 for i = 1:length(T_array2)
     fprintf('Solving for temperature = %d\n', T_array2(i));
     g.TPX = {T_array2(i), P0, X0};
-    g.equilibrate('HP');
-    output = Plug_Flow_1(g, {'NH3', 'N2', 'H2'});
-    X_NH3 = [X_NH3, output(1)];
-    X_N2 = [X_N2, output(2)];
-    X_H2 = [X_H2, output(3)];
+    output = Plug_Flow(g, {'NH3', 'N2', 'H2'}, 1.0);
+    X_NH3 = [X_NH3, output(2, end)];
+    X_N2 = [X_N2, output(3, end)];
+    X_H2 = [X_H2, output(4, end)];
 end
 
 disp(['CPU time = ' num2str(cputime - t0)]);
 
 %% Plot the results
-clf;
+figure(1)
 subplot(1, 2, 1);
 hold on
 plot(T_array1, X_BCL3, 'k');
@@ -97,5 +93,40 @@ axis square
 
 %% Plug-flow preduction of gas-phase concentrations vs. time.
 
+t0 = cputime;
 
+T_array3 = [1125, 1325];
+P0 = 2.0 * 133.32;
+X0 = 'BCL3:0.4,NH3:0.6';
+speciesList = {'BCL3', 'NH3', 'Cl2BNH2', 'ClB(NH2)2', 'B(NH2)3', 'ClBNH', 'HCl'}; 
 
+figure(2)
+
+for i = 1:length(T_array3)
+    fprintf('Solving for temperature = %d\n', T_array3(i));
+    g.TPX = {T_array3(i), P0, X0};
+    output = Plug_Flow(g, speciesList, 0.3);
+    tim = output(1, 1:end);
+    XX_BCL3 = output(2, 1:end);
+    XX_NH3 = output(3, 1:end);
+    XX_ADCB = output(4, 1:end);
+    XX_DACB = output(5, 1:end);
+    XX_TAB = output(6, 1:end);
+    XX_ClBNH = output(7, 1:end);
+    XX_HCL = output(8, 1:end);
+    subplot(1, length(T_array3), i)
+    hold on
+    plot(tim, XX_BCL3, 'k');
+    plot(tim, XX_NH3, '--k');
+    plot(tim, XX_HCL, '--b');
+    plot(tim, XX_ADCB, 'r');
+    plot(tim, XX_DACB, '--r')
+    plot(tim, XX_TAB, 'g');
+    plot(tim, XX_ClBNH, '--g');
+    legend('BCl3', 'NH3', 'HCl', 'ADCB', 'DACB', 'TAB', 'ClBNH');
+    xlabel('Time (s)');
+    ylabel('Mole Fraction');
+    axis square
+end
+
+disp(['CPU time = ' num2str(cputime - t0)]);
--- Cantera/Plug_Flow.m
+++ Cantera/Plug_Flow.m
@@ -1,4 +1,4 @@
-function output = Plug_Flow_1(gas_calc, species_name)
+function output = Plug_Flow(gas_calc, species_name, total_time)
     % Simulation of a plug flow for the BNHCL system.
     % Currently for gas-phase reactions only.
     %
@@ -6,6 +6,8 @@ function output = Plug_Flow_1(gas_calc, species_name)
     %    Object of class 'Solution'.
     % :param species_name:
     %    String or array of string of species names.
+    % :param total_time:
+    %    Total amount of time for simulation. 
     % :return:
     %    Mole fraction of species at the end of 1 second. 
    
@@ -15,19 +17,20 @@ function output = Plug_Flow_1(gas_calc, species_name)
     % create a reactor network and insert the reactor
     network = ReactorNet({r});
 
-    nSteps = 1000;
-    tim = zeros(nSteps);
-    temp = zeros(nSteps);
-    xx = zeros(nSteps, length(species_name));
+    dt = 1.0e-3;
+    nSteps = total_time/dt;
+    tim = zeros(1, nSteps);
+    xx = zeros(length(species_name), nSteps);
+    output = zeros(length(species_name) + 1, nSteps);
     t = 0;
     dt = 1.0e-3;
     for n = 1:nSteps
       t = t + dt;
       network.advance(t);
       tim(n) = network.time;
-      temp(n) = r.T;
-      xx(n, 1:end) = gas_calc.moleFraction(species_name);
+      xx(1:end, n) = gas_calc.moleFraction(species_name);
     end
 
-    output = xx(1000, 1:end);
+    output(1, 1:end) = tim(1:end);
+    output(2:end, 1:end) = xx;
 end
